694f61a7ba5eded8f3cbcea8c4d120b74cb7069e
Jacob-Pang <jacobpang2103@gmail.com>
2023-02-01 19:27:18 +0800

    Remove non ASCII characters from news

--- src/trading/news_listener/__init__.py
+++ src/trading/news_listener/__init__.py
@@ -13,8 +13,11 @@ class News:
         self.description = description
 
     def as_telegram_message(self) -> str:
-        return f"<b>{self.headline}</b>\r{self.timestamp.strftime(r'%b %d, %Y %I:%M %p')} GMT\r" \
+        return re.sub( # Remove non ASCII characters
+            r"[^\x00-\x7F]", '',
+            f"<b>{self.headline}</b>\r{self.timestamp.strftime(r'%b %d, %Y %I:%M %p')} GMT\r" \
                 + f"{self.article_link}\r\r{self.description}"
+        )
 
 class NewsListenerBase (ListenerBase):
     def __init__(self, set_timestamp: datetime.datetime = None, max_capacity: int = 100) -> None:
